4a9d424ae9b31bb091eaebc6c75af26540f5d469
morphar <dan@techba.se>
2016-08-07 12:51:38 +0200

    Add files

---
+++ main.go
@@ -0,0 +1,409 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+
+	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/cheggaaa/pb"
+	"github.com/morphar/rescuelife/minicrawler"
+)
+
+type Media struct {
+	Id             string `json:"id"`
+	MediaType      string `json:"media_type"`
+	Format         string `json:"format"`
+	Processed      bool   `json:"processed"`
+	CreatedAt      int    `json:"created_at"`
+	UpdatedAt      int    `json:"updated_at"`
+	TakenAt        int    `json:"taken_at"`
+	Width          int    `json:"width"`
+	Height         int    `json:"height"`
+	Orientation    int    `json:"orientation"`
+	Privacy        int    `json:"privacy"`
+	IsBestPhoto    bool   `json:"is_best_photo"`
+	TimeZoneOffset int    `json:"time_zone_offset"`
+	Hidden         bool   `json:"hidden"`
+	Visible        bool   `json:"visible"`
+	Filesize       int    `json:"filesize"`
+	BucketId       int    `json:"bucket_id"`
+	Status         string `json:"status"`
+	Retries        int    `json:"retries"`
+}
+
+type APIResponse struct {
+	Status       int     `json:"status"`
+	Media        []Media `json:"media"`
+	Total        int     `json:"total"`
+	Limit        int     `json:"limit"`
+	Offset       int     `json:"offset"`
+	UsingCache   bool    `json:"using_cache"`
+	ResponseTime int     `json:"response_time"`
+}
+
+var (
+	loginUrl    *url.URL
+	signinUrl   *url.URL
+	apiPageUrl  *url.URL
+	apiUrl      *url.URL
+	originalUrl *url.URL
+
+	signinValues url.Values
+
+	accessTokenRE *regexp.Regexp
+	accessToken   string
+
+	pathPerm os.FileMode = 0770
+	filePerm os.FileMode = 0770
+
+	mediaPath string = "picturelife"
+	indexPath string = "pl_index.json"
+
+	// Flags
+	retry bool = false // Retry failed images and videos?
+)
+
+func init() {
+	var err error
+
+	flag.BoolVar(&retry, "retry", retry, "Retry failed images and videos?")
+
+	loginUrl, err = url.Parse("http://picturelife.com/login")
+	if err != nil {
+		panic("Unable to parse login URL")
+	}
+
+	// Login posts to this
+	signinUrl, err = url.Parse("http://picturelife.com/signin")
+	if err != nil {
+		panic("Unable to parse sign in URL")
+	}
+
+	apiPageUrl, err = url.Parse("http://picturelife.com/api")
+	if err != nil {
+		panic("Unable to parse API Page URL")
+	}
+
+	originalUrl, err = url.Parse("http://picturelife.com/d/original/")
+	if err != nil {
+		panic("Unable to parse API Page URL")
+	}
+
+	accessTokenRE = regexp.MustCompile("<script>\\s*pl\\.access_token\\s*=\\s*'([^']+)';\\s*pl\\.api_url\\s*=\\s*'([^']+)'\\s*</script>")
+
+	err = os.MkdirAll(mediaPath, pathPerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if len(os.Args) > 1 && (strings.Contains(os.Args[1], "help") || strings.Contains(os.Args[1], "-h")) {
+		printHelp()
+		return
+	}
+
+	// Instantiate the crawler
+	client := minicrawler.New()
+
+	// Ask for email and password
+	signinValues := getCredentials()
+
+	res := client.GetOrDie(loginUrl.String())
+	res.Body.Close()
+
+	res = client.PostFormOrDie(signinUrl.String(), signinValues)
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	if strings.Contains(string(body), "Login error! Please check your email and password.") {
+		fmt.Println("Login error! Please check your email and password.")
+		return
+	}
+
+	res = client.GetOrDie(apiPageUrl.String())
+	body, err = ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	fmt.Print("Trying to extract Access Token and API URL...")
+	parts := accessTokenRE.FindStringSubmatch(string(body))
+	if len(parts) != 3 {
+		fmt.Println("\nUnable to extract Access Token and API URL.")
+		fmt.Println("This is the source code received:")
+		fmt.Println(string(body))
+		return
+	}
+	fmt.Println(" Done!")
+
+	accessToken = parts[1]
+	apiUrl, err = url.Parse(parts[2])
+	if err != nil {
+		fmt.Println("Unable to parse API Page URL")
+		return
+	}
+
+	// So far, so good... Now extract the index json, if it hasn't already been done
+
+	// If the JSON index file does not exist, we'll fetch it from the API and create it
+
+	if _, err := os.Stat(indexPath); os.IsNotExist(err) {
+		fmt.Println("\nTrying to extract index of all files...")
+
+		var progress *pb.ProgressBar
+		var allMedia []Media
+
+		indexUrl := apiUrl.String() + "/media/index"
+
+		offset := 0
+		limit := 500
+		total := -1
+
+		formValues := url.Values{
+			"taken_at_after":      {"0"},
+			"include_hidden":      {"true"},
+			"show_invisible":      {"true"},
+			"warm_thumbs":         {"false"},
+			"include_names":       {"false"},
+			"include_comments":    {"false"},
+			"include_signature":   {"false"},
+			"include_access_info": {"false"},
+			"include_likes":       {"false"},
+			"offset":              {strconv.Itoa(offset)},
+			"limit":               {strconv.Itoa(limit)},
+			"access_token":        {accessToken},
+		}
+
+		for total == -1 || offset < total {
+			formValues.Set("offset", strconv.Itoa(offset))
+
+			res := client.PostFormOrDie(indexUrl, formValues)
+			body, err = ioutil.ReadAll(res.Body)
+			res.Body.Close()
+
+			var apiResponse APIResponse
+			err := json.Unmarshal(body, &apiResponse)
+			if err != nil {
+				fmt.Println("ERROR! Unable to read JSON response from API. Please try again later.")
+				os.Exit(0)
+			}
+
+			allMedia = append(allMedia, apiResponse.Media...)
+			total = apiResponse.Total
+
+			if progress == nil {
+				progress = pb.New(total)
+				progress.ShowCounters = true
+				progress.ShowTimeLeft = true
+				progress.Start()
+			}
+
+			progress.Set(offset)
+
+			offset += limit
+		}
+
+		progress.FinishPrint("Done fetching JSON index")
+
+		mediaJson, _ := json.Marshal(allMedia)
+		err = ioutil.WriteFile(indexPath, mediaJson, filePerm)
+
+		if err != nil {
+			fmt.Println("ERROR! Unable to write JSON index file to disk. Sorry...")
+			fmt.Println("Please go to GitHub and open an issue.")
+			os.Exit(0)
+		}
+	}
+
+	if _, err := os.Stat(indexPath); os.IsNotExist(err) {
+		fmt.Println("ERROR! Unable to find the JSON index file from disk. Sorry...")
+		fmt.Println("Please go to GitHub and open an issue.")
+		os.Exit(0)
+	}
+
+	src, err := ioutil.ReadFile(indexPath)
+	if err != nil {
+		fmt.Println("ERROR! Unable to read the JSON index file from disk. Sorry...")
+		fmt.Println("Please go to GitHub and open an issue.")
+		os.Exit(0)
+	}
+
+	var allMedia []Media
+
+	json.Unmarshal(src, &allMedia)
+
+	fmt.Println("\nTrying to extract pictures and videos...")
+
+	ch := make(chan bool, 10)
+	mediaLock := sync.Mutex{}
+
+	progressCount := len(allMedia)
+	for _, media := range allMedia {
+		if media.Status == "done" {
+			progressCount -= 1
+		} else if !retry && media.Status == "failed" {
+			progressCount -= 1
+		}
+	}
+
+	progress := pb.New(progressCount)
+	progress.ShowCounters = true
+	progress.ShowTimeLeft = true
+	progress.Start()
+
+	fails := 0
+	success := 0
+	for i, media := range allMedia {
+		if allMedia[i].Status == "done" {
+			success += 1
+			continue
+		}
+
+		if !retry && allMedia[i].Status == "failed" {
+			fails += 1
+			continue
+		}
+
+		ch <- true
+
+		go func(index int, media *Media) {
+			fetchMedia(&client, media)
+			mediaLock.Lock()
+			allMedia[index] = *media
+			if media.Status == "done" {
+				success += 1
+			} else {
+				fails += 1
+			}
+			progress.Increment()
+			mediaLock.Unlock()
+			<-ch
+		}(i, &media)
+
+		if i > 0 && i%10 == 0 {
+			mediaJson, _ := json.Marshal(allMedia)
+			err = ioutil.WriteFile(indexPath, mediaJson, filePerm)
+
+			if err != nil {
+				fmt.Println("ERROR! Unable to write update JSON index file to disk. Sorry...")
+				fmt.Println("Please go to GitHub and open an issue.")
+				os.Exit(0)
+			}
+		}
+	}
+
+	mediaJson, _ := json.Marshal(allMedia)
+	err = ioutil.WriteFile(indexPath, mediaJson, filePerm)
+
+	if err != nil {
+		fmt.Println("ERROR! Unable to write update JSON index file to disk. Sorry...")
+		fmt.Println("Please go to GitHub and open an issue.")
+		os.Exit(0)
+	}
+
+	progress.Finish()
+
+	fmt.Println("Done trying to fetch all pictures and videos.")
+	fmt.Println("Result:")
+	fmt.Println("\tSuccess:", success)
+	fmt.Println("\tFailed: ", fails)
+}
+
+func fetchMedia(client *minicrawler.Crawler, media *Media) {
+	media.Retries += 1
+	media.Status = "started"
+
+	extension := strings.ToLower(media.Format)
+	extension = strings.Replace(extension, "jpeg", "jpg", 1)
+	filename := media.Id + "." + extension
+	filePath := mediaPath + "/" + filename
+	url := originalUrl.String() + media.Id
+
+	out, err := os.Create(filePath)
+	if err != nil {
+		media.Status = "failed"
+		out.Close()
+		os.Remove(filePath)
+		return
+	}
+
+	res, err := client.Client.Get(url)
+	if err != nil || res.StatusCode != 200 {
+		media.Status = "failed"
+		out.Close()
+		if res != nil {
+			res.Body.Close()
+		}
+		os.Remove(filePath)
+		return
+	}
+
+	n, err := io.Copy(out, res.Body)
+	if err != nil {
+		media.Status = "failed"
+		out.Close()
+		res.Body.Close()
+		os.Remove(filePath)
+		return
+	}
+
+	if n != int64(media.Filesize) {
+		media.Status = "failed"
+		out.Close()
+		res.Body.Close()
+		os.Remove(filePath)
+
+	} else {
+		media.Status = "done"
+		out.Close()
+		res.Body.Close()
+	}
+}
+
+func printHelp() {
+	fmt.Println("Currently you can only choose whether or not to retry failed fetches")
+	flag.PrintDefaults()
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println(`./rescuelife -retry=true`)
+	fmt.Println("")
+}
+
+func getCredentials() (signinValues url.Values) {
+	fmt.Println("\n---------------------------------------------------------------------------------------------------------------------")
+	fmt.Println("Your email and password is needed in order to get a cookie, extract Access Token and to fetch your images and videos.")
+	fmt.Println("Nothing will be stored or copied to any other server.")
+	fmt.Println("---------------------------------------------------------------------------------------------------------------------\n")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Your email: ")
+	email, _ := reader.ReadString('\n')
+	email = strings.Trim(email, "\n")
+
+	fmt.Print("Your password: ")
+	bytePassword, _ := terminal.ReadPassword(0)
+	password := strings.Trim(string(bytePassword), "\n")
+	fmt.Println("\n")
+
+	if email == "" || password == "" {
+		fmt.Println("ERROR! Please provide email and password")
+		os.Exit(0)
+	}
+
+	signinValues = url.Values{"email": {email}, "password": {password}}
+
+	return
+}
---
+++ minicrawler/minicrawler.go
@@ -0,0 +1,115 @@
+// minicrawler is a super simple crawler.
+// It's basically a wrapper around http.Client to make some things easier
+// It's sole purpose is to do the fetching in Picture Life Rescuer.
+package minicrawler
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"os"
+	"time"
+)
+
+const (
+	DIE_RETRIES = 10
+	DIE_SLEEP   = 10 * time.Second
+)
+
+type Crawler struct {
+	http.Client
+	UserAgent string
+}
+
+func New() Crawler {
+	jar, _ := cookiejar.New(nil)
+	return Crawler{
+		Client:    http.Client{Jar: jar},
+		UserAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+	}
+}
+
+func (c *Crawler) Get(u string) (res *http.Response, err error) {
+	pUrl, _ := url.Parse(u)
+	res, err = c.Client.Get(u)
+	c.Client.Jar.SetCookies(pUrl, res.Cookies())
+	return res, err
+}
+
+func (c *Crawler) Post(u string, bodyType string, body io.Reader) (res *http.Response, err error) {
+	pUrl, _ := url.Parse(u)
+	res, err = c.Client.Get(u)
+	c.Client.Jar.SetCookies(pUrl, res.Cookies())
+	return c.Client.Post(u, bodyType, body)
+}
+
+func (c *Crawler) PostForm(u string, data url.Values) (res *http.Response, err error) {
+	pUrl, _ := url.Parse(u)
+	res, err = c.Client.Get(u)
+	c.Client.Jar.SetCookies(pUrl, res.Cookies())
+	return c.Client.PostForm(u, data)
+}
+
+func (c *Crawler) GetOrDie(url string) (res *http.Response) {
+	var err error
+
+	fmt.Print("Fetching ", url, "...")
+
+	for i := 0; i < DIE_RETRIES; i++ {
+		res, err = c.Get(url)
+		if err != nil || res.StatusCode != 200 {
+			fmt.Print(".")
+			time.Sleep(DIE_SLEEP)
+		} else {
+			break
+		}
+	}
+
+	if err != nil {
+		fmt.Println("\nFAILED! Picturelife is probably down, so try again later.")
+		os.Exit(0)
+	}
+	fmt.Println(" Done!")
+	return
+}
+
+func (c *Crawler) PostOrDie(url string, bodyType string, body io.Reader) (res *http.Response) {
+	var err error
+
+	for i := 0; i < DIE_RETRIES; i++ {
+		res, err = c.Post(url, bodyType, body)
+		if err != nil || res.StatusCode != 200 {
+			time.Sleep(DIE_SLEEP)
+		} else {
+			break
+		}
+	}
+
+	if err != nil {
+		fmt.Println("\nFAILED! Picturelife is probably down, so try again later.")
+		os.Exit(0)
+	}
+	return
+}
+
+func (c *Crawler) PostFormOrDie(url string, data url.Values) (res *http.Response) {
+	var err error
+
+	for i := 0; i < DIE_RETRIES; i++ {
+		res, err = c.PostForm(url, data)
+		if err != nil || res.StatusCode != 200 {
+			fmt.Print(".")
+			time.Sleep(DIE_SLEEP)
+		} else {
+			break
+		}
+	}
+
+	if err != nil {
+		fmt.Println("\nFAILED! Picturelife is probably down, so try again later.")
+		os.Exit(0)
+	}
+	return
+}
